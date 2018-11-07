b7afc967eb5297f3112f524d21ef4b49eeae9a0b
m0se <mhojmose@hotmail.com>
2018-11-07 11:58:59 +0100

    Joke
    joke

---
+++ models/Joke.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var joke = new Schema({
+    setup: setup,
+    punchline: punchline
+});
+// noinspection JSAnnotator
+joke.methods.toString() = function () {
+    return this.setup + ", punchline: " + this.punchline
+};
+
+module.exports = mongoose.model('Joke', joke);
---
+++ routes/joke.js
@@ -0,0 +1,3 @@
+const controller = require("../controllers/controller");
+const express = require('express');
+const router = express.Router();
